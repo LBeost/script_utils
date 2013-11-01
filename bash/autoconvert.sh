3aec7c6bafb4ef0057b6b926acaff1212e3d821f
LBeost <lbeost@dom.tld>
2013-11-01 16:20:48 +0100

    autoconvert.sh: small update

--- bash/autoconvert.sh
+++ bash/autoconvert.sh
@@ -77,16 +77,21 @@ fi
 
 
 ## main : find media files in order to encode them
-find $workDir -name '*.mp4' -o -name '*.mkv' -o -name '*.avi' | while read f; do
-	newFile="${f%.*}.CONVERTED.mkv"
+find $workDir -name '*.mp4' -o -name '*.mkv' -o -name '*.avi' -o -name '*.wmv' | while read f; do
+	newFile="${f%.*}.SD.mkv"
 	width=$($avconv -i "$f" 2>&1 | gawk '{ match($0,/(Stream #0)(.*)Video:.*, ([0-9]*)x([0-9]*)(.*)/,r); if (RSTART>0) { printf "%s\n", r[3] } }')
-	if [ "$f" == "${f/CONVERTED.mkv/}" ] && [ ! -f "$newFile" ]; then
+	if [ "$f" == "${f/SD.mkv/}" ] && [ ! -f "$newFile" ]; then
 		# no encoded version found
 		if [ $width -gt 1200 ]; then
 			# must re-encode to SD if width > 1200
-			$nice $nicelvl $avconv -y -i "$f" -vcodec libx264 -preset fast -crf 24 \
+			$nice $nicelvl $avconv -y -i "$f" -vcodec libx264 -preset fast -crf 22 \
 				-r 23.976 -s 704x396 -threads 0 -v info -acodec libmp3lame \
-				-b:a 160k -ac 2 -sn "$newFile"
+				-b:a 192k -ac 2 -sn "$newFile" ; returnValue=$?
+		fi
+		# if encoding is successfull, remove high quality version
+		if [ $returnValue == 0 ]; then
+			rm -f "$f"
 		fi
 	fi
 done
+
